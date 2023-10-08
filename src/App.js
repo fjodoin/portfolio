fe007e8a1c4ed391e0e1e3abdb4432186ae9418a
fjodoin <fjodoin@protonmail.com>
2023-10-08 17:34:23 -0400

    added favicon

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
 import './App.css';
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom'; // Import Routes and Route
+import React, { Component } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import About from './About';
 import Contact from './Contact';
 import Home from './Home';
+import favicon from './profileLogo.png';
 
-function App() {
-  return (
-    <div>
-      <header className="header">
-      <nav className="navbar">
-          <ul id="navigation">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-          <div className="navLogo">
-            ᓚᘏᗢ
-          </div>
-        </nav>
-      </header>
-      <Routes> {/* Wrap your Routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.changeFavicon();
+  }
+
+  changeFavicon() {
+    const link = document.querySelector("link[rel*='icon']");
+    if (link) {
+      link.href = favicon;
+    } else {
+      const newLink = document.createElement('link');
+      newLink.rel = 'icon';
+      newLink.href = favicon;
+      document.head.appendChild(newLink);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="header">
+          <nav className="navbar">
+            <ul id="navigation">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+            <div className="navLogo">
+              ᓚᘏᗢ
+            </div>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
