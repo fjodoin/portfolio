9865a99a7a5b53ac5fe13f03b8063592d67539b1
fjodoin <fjodoin@protonmail.com>
2025-01-17 11:23:25 -0500

    fixing protfolio homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link for navigation
 import logo from './logo.svg';
 import overlayLogo from './overlayLogo.svg';
 
@@ -12,33 +13,39 @@ class Home extends React.Component {
                         <img src={logo} className="App-logo" alt="logo" />
                         <img src={overlayLogo} className="overlay-image" alt="overlay" />
                     </div>
-                        <h1>⛅️ Cloud Ethical Hacker 🌩️</h1>
-                        <div>
-                            <div className="skills">
-                                <table className="skills-table">
-                                    <thead>
-                                        <tr>
-                                            <th>Skills</th>
-                                            <th>Details</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>🏴‍☠️ Offensive Cloud Security</td>
-                                            <td>Assumed Breach Penetration Testing</td>
-                                        </tr>
-                                        <tr>
-                                            <td>🏰 Defensive Cloud Security</td>
-                                            <td>SIEM Alert & Monitoring Strategies</td>
-                                        </tr>
-                                        <tr>
-                                            <td>🎯 Integrated Cloud Security </td>
-                                            <td>Purple Teaming & SOC Enhancements</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                    <h1>⛅️ Cloud Ethical Hacker 🌩️</h1>
+                    <div>
+                        <div className="skills">
+                            <table className="skills-table">
+                                <thead>
+                                    <tr>
+                                        <th>Skills</th>
+                                        <th>Details</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <Link to="/offensive-cloud-security">🏴‍☠️ Offensive Cloud Security</Link>
+                                        </td>
+                                        <td>Assumed Breach Penetration Testing</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <Link to="/defensive-cloud-security">🏰 Defensive Cloud Security</Link>
+                                        </td>
+                                        <td>SIEM Alert & Monitoring Strategies</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <Link to="/integrated-cloud-security">🎯 Integrated Cloud Security</Link>
+                                        </td>
+                                        <td>Purple Teaming & SOC Enhancements</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
+                    </div>
                 </header>
             </div>
         );
