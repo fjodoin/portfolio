85744d4f2af5ac04fc5af676659b73b3368a4404
fjodoin <fjodoin@protonmail.com>
2025-01-17 11:09:42 -0500

    fixing protfolio homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import React, { Component } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import About from './About';
-import Contact from './Contact';
 import Home from './Home';
+import Contact from './Contact';
+import OffensiveCloudSecurity from './OffensiveCloudSecurity';
+import DefensiveCloudSecurity from './DefensiveCloudSecurity';
+import IntegratedCloudSecurity from './IntegratedCloudSecurity';
 import favicon from './profileLogo.png';
 
 class App extends Component {
@@ -32,8 +34,13 @@ class App extends Component {
               <li>
                 <Link to="/">Home</Link>
               </li>
-              <li>
-                <Link to="/about">About</Link>
+              <li className="dropdown">
+                <Link to="#">Cloud Security</Link>
+                <ul className="dropdown-content">
+                  <li><Link to="/offensive-cloud-security">Offensive Cloud Security</Link></li>
+                  <li><Link to="/defensive-cloud-security">Defensive Cloud Security</Link></li>
+                  <li><Link to="/integrated-cloud-security">Integrated Cloud Security</Link></li>
+                </ul>
               </li>
               <li>
                 <Link to="/contact">Contact</Link>
@@ -44,9 +51,12 @@ class App extends Component {
             </div>
           </nav>
         </header>
+
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/offensive-cloud-security" element={<OffensiveCloudSecurity />} />
+          <Route path="/defensive-cloud-security" element={<DefensiveCloudSecurity />} />
+          <Route path="/integrated-cloud-security" element={<IntegratedCloudSecurity />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
